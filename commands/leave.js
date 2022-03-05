ea52f126d281364f776d7b7fb67b6e62e558c80f
Siba4738 <64182228+Siba4738@users.noreply.github.com>
2022-03-05 07:43:23 +0530

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -11,8 +11,8 @@ module.exports = {
 
     run: async function (client, message, args) {
         let channel = message.member.voice.channel;
-        if (!channel) return sendError("I'm sorry but you need to be in a voice channel!", message.channel);
-        if (!message.guild.me.voice.channel) return sendError("I Am Not In Any Voice Channel!", message.channel);
+        if (!channel) return sendError("<:x_:944952424270012447> I'm sorry but you need to be in a voice channel!", message.channel);
+        if (!message.guild.me.voice.channel) return sendError("<:x_:944952424270012447> I Am Not In Any Voice Channel!", message.channel);
 
         try {
             await message.guild.me.voice.channel.leave();
@@ -24,10 +24,10 @@ module.exports = {
         const Embed = new MessageEmbed()
             .setAuthor("Leave Voice Channel", "https://github.com/navaneethkm004/my-images/blob/main/giphy.gif?raw=true")
             .setColor("#fffdd0")
-            .setTitle("Success")
-            .setDescription("🎶 Left The Voice Channel.")
+            .setTitle("<a:check_mark:944952166869778502> Success")
+            .setDescription("<a:song12:949469152647733339> Left The Voice Channel.")
             .setTimestamp();
 
-        return message.channel.send(Embed).catch(() => message.channel.send("🎶 Left The Voice Channel :C"));
+        return message.channel.send(Embed).catch(() => message.channel.send("<a:song12:949469152647733339> Left The Voice Channel :C"));
     },
 };
